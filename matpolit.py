5e1e5354decaea04e24ca326e6bf2f37f27f5818
Mohammad parhamde <142143060+parhamde@users.noreply.github.com>
2023-12-08 00:30:37 +0330

    Add files via upload
    تصویر همراه با کد

---
+++ matpolit.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+ax = plt.axes(projection="3d")
+
+x = np.arange(-5, 5, 0.1)
+y = np.arange(-5, 5, 0.1)
+
+X, Y = np.meshgrid(x, y)
+
+Z = np.sin(X) * np.cos(Y)
+
+ax.plot_surface(X , Y, Z , cmap="Spectral")
+ax.set_title("my")
+ax.set_xlabel("text")
+plt.show()
